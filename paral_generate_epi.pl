24706dc72825d18e58132a8bc3dd01bbf47a3229
wyp1125 <wyp1125@gmail.com>
2019-10-07 21:59:47 -0400

    update

--- paral_generate_epi.pl
+++ paral_generate_epi.pl
@@ -3,7 +3,10 @@ for($i=0;$i<4;$i++)
 {
 for($j=$i+1;$j<5;$j++)
 {
-$cmd="python generate_epi.py gt/$a[$i]_$a[$j].gt epi/$a[$i]_$a[$j].epi 1";
+$cmd="python3 generate_epi.py gt/$a[$i]_$a[$j].gt epi/$a[$i]_$a[$j].epi 0";
+print $cmd,"\n";
+$b=`$cmd`;
+$cmd="python3 generate_epi.py gt/$a[$i]_$a[$j].gt epi/$a[$j]_$a[$i].epi 1";
 print $cmd,"\n";
 $b=`$cmd`;
 }
